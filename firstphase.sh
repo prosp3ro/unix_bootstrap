e2d7390668dea984aeadfaab1e974c8700f81f7f
prospero <g0lden_vo1d@protonmail.com>
2023-05-04 15:10:52 +0200

    apache

--- firstphase.sh
+++ firstphase.sh
@@ -67,3 +67,8 @@ cd "$HOME/tmp/neovim" || exit
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 # git checkout stable
 sudo make install
+
+sudo sed -i 's/;\(extension=gd\)/\1/' /etc/php/php.ini
+sudo sed -i 's/#\(LoadModule rewrite_module modules\/mod_rewrite\.so\)/\1/' /etc/httpd/conf/httpd.conf
+sudo sed -i '/^LoadModule mpm_event_module modules\/mod_mpm_event\.so/s/^/#/' /etc/httpd/conf/httpd.conf
+sudo sed -i '/^#LoadModule mpm_prefork_module modules\/mod_mpm_prefork.so\.so/s/^#//' /etc/httpd/conf/httpd.conf
