8471aabf2f064eb64206281233b7e42acda03ce6
prospero <g0lden_vo1d@protonmail.com>
2023-09-16 15:45:24 +0200

    sync

--- init.sh
+++ init.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-sudo pacman -Syy
+sudo pacman -Sy
 sudo pacman -S archlinux-keyring
-sudo pacman -Syy
 
-packages=(
+packages1=(
     "linux"
     "linux-firmware"
     "vim"
@@ -34,25 +33,104 @@ packages=(
     "xf86-video-intel"
 )
 
-sudo pacman -S "${packages[*]}"
+sudo pacman -S ${packages1[*]}
 
 systemctl enable --now NetworkManager
 systemctl enable --now bluetooth
 systemctl enable --now cups
 
-sudo pacman -S xorg-server xorg-xwininfo xorg-setxkbmap i3-wm i3blocks i3lock i3status \
-    polkit ttf-font-awesome ttf-dejavu xcompmgr xorg-xprop arandr \
-    exfat-utils python-qdarkstyle openssh noto-fonts noto-fonts-emoji \
-    ntfs-3g pulsemixer pavucontrol maim unclutter unzip xcape xclip \
-    yt-dlp zathura zathura-pdf-mupdf poppler fzf \
-    xorg-xbacklight firefox zsh system-config-printer gparted \
-    ueberzug mediainfo bat ffmpeg ffmpegthumbnailer \
-    odt2txt atool zsh-autosuggestions mpd mpv mpc \
-    ncmpcpp dunst libnotify python python-pip libreoffice skim \
-    socat moreutils alacritty blueman cmake findutils gawk \
-    ripgrep gimp gtk2 gtk3 gtk4 highlight less lxappearance lynx \
-    lua pinentry qt5ct tar smbclient sed unrar zip unzip tzdata wget whois \
-    xwallpaper ninja curl ttf-linux-libertine ttf-font-awesome ttf-dejavu
+packages2=(
+    "xorg-server"
+    "xorg-xwininfo"
+    "xorg-setxkbmap"
+    "i3-wm"
+    "i3blocks"
+    "i3lock"
+    "i3status"
+    "polkit"
+    "ttf-font-awesome"
+    "ttf-dejavu"
+    "xcompmgr"
+    "xorg-xprop"
+    "arandr"
+    "exfat-utils"
+    "python-qdarkstyle"
+    "openssh"
+    "noto-fonts"
+    "noto-fonts-emoji"
+    "ntfs-3g"
+    "pulsemixer"
+    "pavucontrol"
+    "maim"
+    "unclutter"
+    "unzip"
+    "xcape"
+    "xclip"
+    "yt-dlp"
+    "zathura"
+    "zathura-pdf-mupdf"
+    "poppler"
+    "fzf"
+    "xorg-xbacklight"
+    "firefox"
+    "zsh"
+    "system-config-printer"
+    "gparted"
+    "ueberzug"
+    "mediainfo"
+    "bat"
+    "ffmpeg"
+    "ffmpegthumbnailer"
+    "odt2txt"
+    "atool"
+    "zsh-autosuggestions"
+    "mpd"
+    "mpv"
+    "mpc"
+    "ncmpcpp"
+    "dunst"
+    "libnotify"
+    "python"
+    "python-pip"
+    "libreoffice"
+    "skim"
+    "socat"
+    "moreutils"
+    "alacritty"
+    "blueman"
+    "cmake"
+    "findutils"
+    "gawk"
+    "ripgrep"
+    "gimp"
+    "gtk2"
+    "gtk3"
+    "gtk4"
+    "highlight"
+    "less"
+    "lxappearance"
+    "lynx"
+    "lua"
+    "pinentry"
+    "qt5ct"
+    "tar"
+    "smbclient"
+    "sed"
+    "unrar"
+    "zip"
+    "unzip"
+    "tzdata"
+    "wget"
+    "whois"
+    "xwallpaper"
+    "ninja"
+    "curl"
+    "ttf-linux-libertine"
+    "ttf-font-awesome"
+    "ttf-dejavu"
+)
+
+sudo pacman -S ${packages2[*]}
 
 mkdir "$HOME/.cache"
 mkdir -p "$HOME/.local/share"
