b77f03d2fe9ad3aecd31896746ed6c2ca014c4e9
prosp3ro <g0lden_vo1d@protonmail.com>
2023-10-30 19:30:00 +0100

    sync

--- apache.sh
+++ apache.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-sudo pacman -S apache
+sudo pacman -S apache php-fpm
 
 sudo systemctl enable --now httpd
+sudo systemctl enable --now php-fpm
 
 # sudo sed -i 's/#\(LoadModule rewrite_module modules\/mod_rewrite\.so\)/\1/' /etc/httpd/conf/httpd.conf
 # sudo sed -i '/^LoadModule mpm_event_module modules\/mod_mpm_event\.so/s/^/#/' /etc/httpd/conf/httpd.conf
--- nginx.sh
+++ nginx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -S nginx
+sudo pacman -S nginx php-fpm
 
 sudo systemctl enable --now nginx
 sudo systemctl enable --now php-fpm
