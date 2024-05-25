66aa73020054cf231496a510f14089c40085bcfd
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2024-05-25 11:06:32 +0200

    use paru binary instead of compiling

--- 2-aur.sh
+++ 2-aur.sh
@@ -7,7 +7,12 @@ PARU_TMP_DIR="$HOME/tmp/paru"
 
 sudo pacman -S --needed base-devel
 
-git clone https://aur.archlinux.org/paru.git "$PARU_TMP_DIR" || exit 1
+# git clone https://aur.archlinux.org/paru "$PARU_TMP_DIR" || exit 1
+# cd "$PARU_TMP_DIR" || exit 1
+# makepkg -si
+# [ -d "$PARU_TMP_DIR" ] && rm -rf "$PARU_TMP_DIR"
+
+git clone https://aur.archlinux.org/paru-bin "$PARU_TMP_DIR" || exit 1
 cd "$PARU_TMP_DIR" || exit 1
 makepkg -si
 [ -d "$PARU_TMP_DIR" ] && rm -rf "$PARU_TMP_DIR"
