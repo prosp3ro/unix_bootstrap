8bf0f08db523e237b43ee8f2bf298782e2783e20
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2024-05-30 13:24:28 +0200

    postgres & php

--- dev/php.sh
+++ dev/php.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+echo "Make sure you have composer config dotfile first."
+read -rp "Continue? [y/N] " answer
+
+if [ "$answer" != "y" ]; then
+   exit 
+fi
+
 sudo pacman -S composer php php-gd
 
-# sudo sed -i 's/;\(extension=gd\)/\1/' /etc/php/php.ini
+sudo sed -i 's/;\(extension=gd\)/\1/' /etc/php/php.ini
 
 composer global require
--- dev/postgres.sh
+++ dev/postgres.sh
@@ -3,3 +3,6 @@
 sudo pacman -S postgres php-pgsql
 su -l postgres -c "initdb --locale=C.UTF-8 --encoding=UTF8 -D '/var/lib/postgres/data'"
 su -l postgres -c "createuser --interactive"
+
+sudo sed -i 's/;\(extension=pdo_pgsql\)/\1/' /etc/php/php.ini
+sudo sed -i 's/;\(extension=pgsql\)/\1/' /etc/php/php.ini
