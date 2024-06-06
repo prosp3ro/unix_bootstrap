a18c42a444e38a685d03fd87846146b9075098ec
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2024-06-06 10:16:33 +0200

    sync

--- dev/postgres.sh
+++ dev/postgres.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-sudo pacman -S postgres php-pgsql
-su -l postgres -c "initdb --locale=C.UTF-8 --encoding=UTF8 -D '/var/lib/postgres/data'"
-su -l postgres -c "createuser --interactive"
+sudo pacman -S postgresql php-pgsql
+sudo su -l postgres -c "initdb --locale=C.UTF-8 --encoding=UTF8 -D '/var/lib/postgres/data'"
+sudo systemctl enable --now postgresql
+sudo su -l postgres -c "createuser --interactive"
 
 sudo sed -i 's/;\(extension=pdo_pgsql\)/\1/' /etc/php/php.ini
 sudo sed -i 's/;\(extension=pgsql\)/\1/' /etc/php/php.ini
-
-sudo systemctl enable --now postgresql
