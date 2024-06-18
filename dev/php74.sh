12b5635cc1cd178c9691086619650a03ec9c11c5
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2024-06-18 14:03:43 +0200

    sync

--- dev/php74.sh
+++ dev/php74.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo id
+
 paru -S php74
 paru -S php74-mbstring
 paru -S php74-phar
@@ -14,5 +16,7 @@ paru -S php74-dom
 paru -S php74-pdo
 paru -S php74-pear
 paru -S php74-xmlwriter
+paru -S php74-ctype
+paru -S php74-simplexml
 
 sudo sed -i 's/;\(extension=gd\)/\1/' /etc/php74/php.ini
